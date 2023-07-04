ad01328717736024a4e3e055897a50e378e06635
ArmijosThais <tarmijos02@gmail.com>
2023-07-04 10:46:38 -0500

    Servicio Listar Socios Cooperativa

---
+++ listarSociosCooperativa.php
@@ -0,0 +1,33 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-with, Content-type, Authorization');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+
+include 'conexionBDRemota.php';
+
+$id_cooperativa = $_POST['id_cooperativa'];
+
+$sql = "SELECT * FROM Usuarios WHERE id_coop IN(SELECT id_cooperativa FROM Cooperativas WHERE id_cooperativa='$id_cooperativa') AND tipo_usuario='socio'";
+
+$resultado = $conexion->query($sql);
+
+if ($resultado->num_rows > 0) {
+    $lista = array();
+
+    while ($fila = $resultado->fetch_assoc()) {
+        $lista[] = $fila;
+    }
+
+    $json = json_encode($lista, JSON_UNESCAPED_UNICODE);
+
+    echo $json;
+} else {
+
+    echo json_encode(array('mensaje' => 'No se encontraron registros en la tabla'));
+}
+
+$conexion->close();
+
+
+?>
