62104a01bec92ee5b19f1b3f322718661324d466
YadiraAllauca <94416577+YadiraAllauca@users.noreply.github.com>
2023-07-09 14:11:17 -0500

    Servicio generarVenta.php corregido con lo solicitado en el issue #17

---
+++ generarVenta.php
@@ -0,0 +1,28 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-with, Content-type, Authorization');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+
+include 'conexionBDRemota.php';
+
+$id_comprador = $_POST['id_comprador'];
+$id_viaje_pertenece = $_POST['id_viaje_pertenece'];
+$id_parada_pertenece = $_POST['id_parada_pertenece'];
+$fecha_venta = $_POST['fecha_venta'];
+$id_forma_pago = $_POST['id_forma_pago'];
+$total_venta = $_POST['total_venta'];
+$codigo_qr_venta = $_POST['codigo_qr_venta'];
+$comprobante_venta = $_POST['comprobante_venta'];
+
+$sql = "INSERT INTO Ventas(id_comprador, id_viaje_pertenece, id_parada_pertenece, fecha_venta, id_forma_pago, estado_venta, total_venta, codigo_qr_venta, comprobante_venta) VALUES
+('$id_comprador', '$id_viaje_pertenece', '$id_parada_pertenece', '$fecha_venta', '$id_forma_pago', 0, '$total_venta', '$codigo_qr_venta', '$comprobante_venta')";
+
+if ($conexion->query($sql) === TRUE) {
+    $id_compra_generada = $conexion->insert_id;
+    echo json_encode(array('id_compra' => $id_compra_generada));
+} else {
+    echo json_encode(array('errorMsg' => $sql . $conexion->error));
+}
+
+$conexion->close();
